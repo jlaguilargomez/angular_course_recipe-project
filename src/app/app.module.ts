19f131c867d58193aaf00dbfaacd0ce40f05b84a
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-17 11:06:32 +0100

    added new features to the recipe-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    FooterComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  public recipes:Recipe[] = [
+    new Recipe('A test recipe','This is a test','https://cdn.pixabay.com/photo/2017/03/26/11/53/food-platter-2175326_960_720.jpg'), new Recipe('Cosa','Güena','https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
