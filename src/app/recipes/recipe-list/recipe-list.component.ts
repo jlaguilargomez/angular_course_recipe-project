af86d3b02e23d488f3dff02e1c1e00bdea9049a0
Jose Luis Aguilar Gómez <jlagomez@minsait.com>
2020-01-30 11:54:42 +0100

    recipe service created / now the list of recipes comes from it

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
     selector: 'app-recipe-list',
@@ -7,24 +8,15 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+    public recipes: Recipe[];
+
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-    public recipes: Recipe[] = [
-        new Recipe(
-            'A test recipe',
-            'This is a test',
-            'https://cdn.pixabay.com/photo/2017/03/26/11/53/food-platter-2175326_960_720.jpg'
-        ),
-        new Recipe(
-            'Cosa',
-            'Güena',
-            'https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg'
-        ),
-    ];
+    constructor(private _recipesServices: RecipesService) {}
 
-    constructor() {}
-
-    ngOnInit() {}
+    ngOnInit() {
+        this.recipes = this._recipesServices.recipes;
+    }
 
     onRecipeSelected(recipe: Recipe) {
         this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class RecipesService {
+    private _recipes: Recipe[] = [
+        new Recipe(
+            'A test recipe',
+            'This is a test',
+            'https://cdn.pixabay.com/photo/2017/03/26/11/53/food-platter-2175326_960_720.jpg'
+        ),
+        new Recipe(
+            'Cosa',
+            'Güena',
+            'https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg'
+        ),
+    ];
 
-  constructor() { }
+    get recipes() {
+        return this._recipes.slice();
+    }
+
+    constructor() {}
 }
