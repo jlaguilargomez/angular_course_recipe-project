0f20551914b8ab6a772c3b554a95d81e1db559b2
Jose Luis Aguilar <jlaguigom@gmail.com>
2020-03-13 08:16:50 +0100

    everything works

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,30 +1,31 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
 
-// import { RecipesComponent } from './recipes.component';
-// import { AuthGuard } from '../auth/auth.guard';
-// import { RecipeStartComponent } from './recipe-start/recipe-start.component';
-// import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-// import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
-// const routes: Routes = [
-//   {
-//     path: 'recipes',
-//     component: RecipesComponent,
-//     canActivate: [AuthGuard],
-//     children: [
-//       { path: '', component: RecipeStartComponent },
-//       // ojo, el orden en el que colocamos las rutas importa para no generar conflictos con parametros opcionales
-//       { path: 'new', component: RecipeEditComponent },
-//       { path: ':id', component: RecipeDetailComponent },
-//       { path: ':id/edit', component: RecipeEditComponent },
-//     ],
-//   },
-// ];
+import { ModuleWithProviders } from '@angular/core';
 
-// @NgModule({
-//   // en este caso llamamos al método "forChild" que es el encargado de cargar las rutas en los módulos dependientes del anterior
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule],
-// })
-// export class RecipesRoutingModule {}
+const subRoutes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      // ojo, el orden en el que colocamos las rutas importa para no generar conflictos con parametros opcionales
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
+    ],
+  },
+];
+
+@NgModule({
+  // en este caso llamamos al método "forChild" que es el encargado de cargar las rutas en los módulos dependientes del anterior
+  imports: [RouterModule],
+  exports: [RouterModule.forChild(routes)],
+})
+export class RecipesRoutingModule {}
